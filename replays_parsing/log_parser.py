811ba92caa16f09b38e691649bc54db314da91ab
danicfortier <danic-soccer@hotmail.com>
2019-09-02 15:00:13 -0400

    added another method that parses a more clean version of the battle

--- replays_parsing/log_parser.py
+++ replays_parsing/log_parser.py
@@ -1,7 +1,5 @@
 from typing import List
-
 from replays_fetching.replay_fetcher import ReplayFetcher
-
 import re
 
 
@@ -24,15 +22,32 @@ class LogParser:
         null = None
         string = logs
         x = string["log"].split("\n")
+        data = []
+        pattern1 = r"\|switch\|"
+        pattern2 = r"\|move\|"
+        pattern3 = r"\|turn\|"
+        pattern4 = r"\|win\|"
+        pattern5 = r"\|faint\|"
+        # keep what starts with those patterns
+        for i in range(len(x)):
+            if re.match(pattern1, x[i]) or re.match(pattern2, x[i]) or re.match(pattern3, x[i]) or \
+                    re.match(pattern4, x[i]) or re.match(pattern5, x[i]):
+                data.append(x[i])
+        return data
+
+    def __parse_logs_with_battle_output(self, logs):
+        null = None
+        string = logs
+        x = string["log"].split("\n")
         pattern1 = r"\|j\|"
         pattern2 = r"\|c\|"
         pattern3 = r"\|l\|"
         pattern4 = r"\|n\|"
         pattern5 = r"\|inactive\|"
         battle = []
+        # only keeping what doesn't start with those patterns
         for i in range(len(x)):
             if not re.match(pattern1, x[i]) and not re.match(pattern2, x[i]) and not re.match(pattern3,x[i]) \
                     and not re.match(pattern4, x[i]) and not re.match(pattern5, x[i]):
                 battle.append(x[i])
-
-        return battle
+            return battle
