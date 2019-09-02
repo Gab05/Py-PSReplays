0386c467c3e59a1910f21c14326b5845a98038c3
danicfortier <danic-soccer@hotmail.com>
2019-09-02 11:54:46 -0400

    Parsing logs

--- replays_parsing/log_parser.py
+++ replays_parsing/log_parser.py
@@ -2,6 +2,8 @@ from typing import List
 
 from replays_fetching.replay_fetcher import ReplayFetcher
 
+import re
+
 
 class LogParser:
     def __init__(self):
@@ -19,4 +21,18 @@ class LogParser:
 
     def __parse_logs(self, logs) -> List[str]:
         # TODO [DanicPy]: Parse logs before returning!
-        return logs
+        null = None
+        string = logs
+        x = string["log"].split("\n")
+        pattern1 = r"\|j\|"
+        pattern2 = r"\|c\|"
+        pattern3 = r"\|l\|"
+        pattern4 = r"\|n\|"
+        pattern5 = r"\|inactive\|"
+        battle = []
+        for i in range(len(x)):
+            if not re.match(pattern1, x[i]) and not re.match(pattern2, x[i]) and not re.match(pattern3,x[i]) \
+                    and not re.match(pattern4, x[i]) and not re.match(pattern5, x[i]):
+                battle.append(x[i])
+
+        return battle
